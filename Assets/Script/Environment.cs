f1a9f36f49a4148e2d19d5c62e267369d73f0cdc
Bebe Cartal <olim.latrac.53@gmail.com>
2022-10-07 16:31:09 +0200

    L'environement suit

---
+++ Assets/Script/Environment.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environemment : MonoBehaviour
+{
+    private GameObject player; //Déclaration du player
+    private Vector3 offset = new Vector3(0, 5, -10); //initialisation de la position de la caméra derrière le player
+
+    void LateUpdate()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); //On récupère position du player
+        transform.position = player.transform.position + offset; // On daplace la caméra en fonction de la position du player
+    }
+}
