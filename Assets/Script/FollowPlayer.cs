67edbf2d8152edbd3a15e6f9504e9a3ddd24c6a0
Baptiste <baptiste.tarrit2@gmail.com>
2022-10-07 10:30:26 +0200

    add cockpit view
    on peut changer de cam maintenant !!!!

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour //Script de suivit du joueur
 {
     private GameObject player; //Déclaration du player
-    private Vector3 offset = new Vector3(3.3f, 2.04f, -4.04f); //initialisation de la position de la caméra derrière le player
-    
+    public GameObject[] tab_waypoint;//initialisation de la position de la caméra derrière le player
+    Transform pos_cam;
+    int target;
+
+
+    private void Start()
+    {
+        pos_cam = tab_waypoint[0].transform;
+    }
+
     void LateUpdate()
     {
         player = GameObject.FindGameObjectWithTag("Player"); //On récupère position du player
-        transform.position = player.transform.position + offset; // On daplace la caméra en fonction de la position du player
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            target = (target + 1) % tab_waypoint.Length;
+            pos_cam = tab_waypoint[target].transform;
+        }
+        transform.position =  pos_cam.position;
+        transform.rotation =  pos_cam.rotation;
     }
 }
